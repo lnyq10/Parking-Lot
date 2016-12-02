bc7a9876d69da85cc05c59c414803eff22513aaf
FerrariFA <ferrarifa@sjtu.edu.cn>
2016-12-02 22:44:09 +0800

    init Rocket anchor

--- rocket.cpp
+++ rocket.cpp
@@ -5,34 +5,42 @@ Rocket::Rocket(float scale, float _x, float _y) {
 
     Poly head_tri( {Vec(0,1), Vec(0.4, 0.7), Vec(-0.4, 0.7)} );
     head_tri.randomColor();
+    head_tri.setAnchor(anchor);
     elem.push_back(head_tri);
 
     Poly body( {Vec(0.4, 0.7), Vec(0.4, -0.7), Vec(-0.4, -0.7), Vec(-0.4, 0.7)} );
     body.randomColor();
+    body.setAnchor(anchor);
     elem.push_back(body);
 
     Poly tail( {Vec(-0.32, -0.7), Vec(-0.38, -0.8), Vec(0.38, -0.8), Vec(0.32, -0.7)} );
     tail.randomColor();
+    tail.setAnchor(anchor);
     elem.push_back(tail);
 
     Poly leftWing( {Vec(-0.4, -0.7), Vec(-0.4, 0), Vec(-0.6, -0.7)} );
     leftWing.randomColor();
+    leftWing.setAnchor(anchor);
     elem.push_back(leftWing);
 
     Poly rightWing( {Vec(0.4, -0.7), Vec(0.4, 0), Vec(0.6, -0.7)} );
     rightWing.fillColor(leftWing);
+    rightWing.setAnchor(anchor);
     elem.push_back(rightWing);
 
     Poly leftWindow( {Vec(-0.2, 0.3), Vec(-0.3, 0.3), Vec(-0.3, 0.4), Vec(-0.2, 0.4)} );
     leftWindow.randomColor();
+    leftWindow.setAnchor(anchor);
     elem.push_back(leftWindow);
 
     Poly rightWindow( {Vec(0.2, 0.3), Vec(0.3, 0.3), Vec(0.3, 0.4), Vec(0.2, 0.4)} );
     rightWindow.fillColor(leftWindow);
+    rightWindow.setAnchor(anchor);
     elem.push_back(rightWindow);
 
     Poly door( {Vec(0.1, 0.2), Vec(0.1, -0.2), Vec(-0.1, -0.2), Vec(-0.1, 0.2)} );
     door.randomColor();
+    door.setAnchor(anchor);
     elem.push_back(door);
 
     zoom(scale);
