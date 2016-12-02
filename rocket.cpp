b19dde20ffe5e5a46540386e830bd79bd890bccf
FerrariFA <ferrarifa@sjtu.edu.cn>
2016-12-02 13:45:51 +0800

    Make rocket

---
+++ rocket.cpp
@@ -0,0 +1,37 @@
+#include "rocket.h"
+
+Rocket::Rocket(float _x, float _y) {
+    anchor = Vec(_x, _y);
+
+    Poly head_tri( {Vec(0,1), Vec(0.4, 0.7), Vec(-0.4, 0.7)} );
+    head_tri.randomColor();
+    elem.push_back(head_tri);
+
+    Poly body( {Vec(0.4, 0.7), Vec(0.4, -0.7), Vec(-0.4, -0.7), Vec(-0.4, 0.7)} );
+    body.randomColor();
+    elem.push_back(body);
+
+    Poly tail( {Vec(-0.32, -0.7), Vec(-0.38, -0.8), Vec(0.38, -0.8), Vec(0.32, -0.7)} );
+    tail.randomColor();
+    elem.push_back(tail);
+
+    Poly leftWing( {Vec(-0.4, -0.7), Vec(-0.4, 0), Vec(-0.6, -0.7)} );
+    leftWing.randomColor();
+    elem.push_back(leftWing);
+
+    Poly rightWing( {Vec(0.4, -0.7), Vec(0.4, 0), Vec(0.6, -0.7)} );
+    rightWing.randomColor();
+    elem.push_back(rightWing);
+
+    Poly leftWindow( {Vec(-0.2, 0.3), Vec(-0.3, 0.3), Vec(-0.3, 0.4), Vec(-0.2, 0.4)} );
+    leftWindow.randomColor();
+    elem.push_back(leftWindow);
+
+    Poly rightWindow( {Vec(0.2, 0.3), Vec(0.3, 0.3), Vec(0.3, 0.4), Vec(0.2, 0.4)} );
+    rightWindow.randomColor();
+    elem.push_back(rightWindow);
+
+    Poly door( {Vec(0.1, 0.1), Vec(0.1, -0.1), Vec(-0.1, -0.1), Vec(-0.1, 0.1)} );
+    door.randomColor();
+    elem.push_back(door);
+}
---
+++ rocket.h
@@ -0,0 +1,11 @@
+#ifndef PARKING_LOT_ROCKET_H
+#define PARKING_LOT_ROCKET_H
+
+#include "fig.h"
+
+class Rocket : public Group{
+public:
+    Rocket(float _x = 0.0, float _y = 0.0);
+};
+
+#endif //PARKING_LOT_ROCKET_H
