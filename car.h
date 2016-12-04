80175b491beb83d91a06f83d16cb2b0e0557d7cf
lnyq10 <lnyq10@sjtu.edu.cn>
2016-12-04 22:36:21 +0800

    Update car.h

--- car.h
+++ car.h
@@ -3,9 +3,12 @@
 
 class Car: public Group{
 private:
+    float rotate_angle;
     Poly* pool_1; Poly* pool_2; Poly* pool_3; Poly* pool_4;
 public: 
     Car(float x,float y);
     void rotate_wheel(float angle);
+    void rotate(float A);
+    float get_rotate_angle(){ return rotate_angle; }
     ~Car(){};
 };
