2377e13ef1a8acca5ecf18849b6d94449b68a3b7
FerrariFA <ferrarifa@sjtu.edu.cn>
2016-12-05 22:54:39 +0800

    modify the field

--- Field.cpp
+++ Field.cpp
@@ -1,26 +1,26 @@
 #include "Field.h"
 
 Field::Field() {
-    Line door({Vec(-0.95, -0.99), Vec(-0.75, -0.99)});
+    Line door({Vec(-0.95, -0.5), Vec(-0.75, -0.5)});
     door.fillColor(1,0,0);
-    door.setAnchor(Vec(-0.75, -0.99));
+    door.setAnchor(Vec(-0.75, -0.5));
     lines.push_back(door);
 
-    Line wall({Vec(-0.75, -0.99), Vec(-0.75, -0.8), Vec(0.99, -0.8), Vec(0.99, 0.99), Vec(-0.99, 0.99), Vec(-0.99, -0.99)});
+    Line wall({Vec(-0.75, -0.5), Vec(-0.75, -0.3), Vec(0.99, -0.3), Vec(0.99, 0.99), Vec(-0.99, 0.99), Vec(-0.99, -0.99)});
     wall.fillColor(0, 0, 0);
     lines.push_back(wall);
 
     for (int i = 0; i < 6; ++i){
-        Line down( {Vec(-0.75+i*0.3, -0.8), Vec(-0.75+i*0.3, -0.5)} );
+        Line down( {Vec(-0.75+i*0.3, -0.3), Vec(-0.75+i*0.3, -0.05)} );
         down.fillColor(0,0,0);
         lines.push_back(down);
 
-        Line up( {Vec(-0.75+i*0.3, 0.7), Vec(-0.75+i*0.3, 0.99)} );
+        Line up( {Vec(-0.75+i*0.3, 0.75), Vec(-0.75+i*0.3, 0.99)} );
         up.fillColor(0,0,0);
         lines.push_back(up);
     }
 
-    Poly barrier({Vec(-0.7,-0.1), Vec(0.6,-0.1), Vec(0.6,0.1), Vec(-0.7,0.1),});
+    Poly barrier({Vec(-0.7,0.3), Vec(0.6,0.3), Vec(0.6,0.4), Vec(-0.7,0.4)});
     barrier.randomColor();
     elem.push_back(barrier);
 
