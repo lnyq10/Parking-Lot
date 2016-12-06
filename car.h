31169e1cdf77f942a983c1a59e9ff0d82e49ec47
TimothyGe <ji_getianyi@sjtu.edu.cn>
2016-12-06 19:39:10 +0800

    Update car.h

--- car.h
+++ car.h
@@ -6,7 +6,7 @@ private:
     float rotate_angle;
     Poly* pool_1; Poly* pool_2; Poly* pool_3; Poly* pool_4;
 public: 
-    Car(float x,float y);
+    Car(float x,float y,float angle=PI/2);
     float rotate_wheel(float angle);
     float rotate(float A);
     float get_rotate_angle(){ return rotate_angle; }
